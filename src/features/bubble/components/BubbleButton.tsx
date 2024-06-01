05b53120c1c1bc1d0eeab2c527c78ac57f7c5e86
Xin Shu <xinshu2023@u.northwestern.edu>
2024-06-01 17:08:20 -0500

    add popup message

--- src/features/bubble/components/BubbleButton.tsx
+++ src/features/bubble/components/BubbleButton.tsx
@@ -82,8 +82,8 @@ export const BubbleButton = (props: Props) => {
             stroke: props.iconColor ?? defaultIconColor,
           }}
           class={`stroke-2 fill-transparent absolute duration-200 transition ` + (props.isBotOpened ? 'scale-0 opacity-0' : 'scale-100 opacity-100')}
-          width={buttonSize * 0.6}
-          height={buttonSize * 0.6}
+          width={buttonSize * 1}
+          height={buttonSize * 1}
         >
           <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z" />
         </svg>
@@ -93,8 +93,8 @@ export const BubbleButton = (props: Props) => {
           src={props.customIconSrc}
           class={'rounded-full object-cover' + (props.isBotOpened ? 'scale-0 opacity-0' : 'scale-100 opacity-100')}
           style={{
-            width: `${buttonSize * 1}px`,
-            height: `${buttonSize * 1}px`,
+            width: `${buttonSize * 0.6}px`,
+            height: `${buttonSize * 0.6}px`,
           }}
           alt="Bubble button icon"
         />
@@ -105,7 +105,7 @@ export const BubbleButton = (props: Props) => {
           style={{
             'text-align': 'left', // Correctly using camelCase and colon for property definition
             top: '-100px', // Adjust this value to position the popup above the button
-            left: '-150px',
+            left: '-170px',
             // transform: 'translateX(-50%)',
             width: '200 px', // Set the width of the popup
             height: 'auto', // Adjust height as necessary, 'auto' for content-based height
