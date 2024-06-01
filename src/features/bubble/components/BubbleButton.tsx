45ed67f9717f80b2e2f5ce62d88b0dba3b416917
Xin Shu <xinshu2023@u.northwestern.edu>
2024-06-01 14:18:39 -0500

    add popup message

--- src/features/bubble/components/BubbleButton.tsx
+++ src/features/bubble/components/BubbleButton.tsx
@@ -83,11 +83,10 @@ export const BubbleButton = (props: Props) => {
           style={{
             'text-align': 'left', // Correctly using camelCase and colon for property definition
             top: '-50px', // Adjust this value to position the popup above the button
-            left: '-100px',
+            left: '-150px',
             // transform: 'translateX(-50%)',
             width: '200 px', // Set the width of the popup
             height: 'auto', // Adjust height as necessary, 'auto' for content-based height
-
             // 'max-width': '90%', // Ensures the popup does not exceed the screen width on small devices
           }}
         >
