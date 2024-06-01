f2b176324152fac40d6ab933287482a451682db0
Xin Shu <xinshu2023@u.northwestern.edu>
2024-06-01 13:21:34 -0500

    add popup message

--- src/features/bubble/components/BubbleButton.tsx
+++ src/features/bubble/components/BubbleButton.tsx
@@ -74,6 +74,11 @@ export const BubbleButton = (props: Props) => {
         cursor: props.dragAndDrop ? 'grab' : 'pointer',
       }}
     >
+      {/* Persistent Popup Message */}
+      <div class="absolute bg-white text-black text-sm px-4 py-2 rounded shadow-lg" style={{ bottom: `${buttonSize + 8}px`, left: `50%`, transform: 'translateX(-50%)' }}>
+        This is the bubble button tooltip!
+      </div>
+
       <Show when={isNotDefined(props.customIconSrc)} keyed>
         <svg
           viewBox="0 0 24 24"
@@ -98,20 +103,6 @@ export const BubbleButton = (props: Props) => {
           alt="Bubble button icon"
         />
       </Show>
-
-      <svg
-        viewBox="0 0 24 24"
-        style={{ fill: props.iconColor ?? 'white' }}
-        class={`absolute duration-200 transition ` + (props.isBotOpened ? 'scale-100 rotate-0 opacity-100' : 'scale-0 -rotate-180 opacity-0')}
-        width={buttonSize * 0.6}
-        height={buttonSize * 0.6}
-      >
-        <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          d="M18.601 8.39897C18.269 8.06702 17.7309 8.06702 17.3989 8.39897L12 13.7979L6.60099 8.39897C6.26904 8.06702 5.73086 8.06702 5.39891 8.39897C5.06696 8.73091 5.06696 9.2691 5.39891 9.60105L11.3989 15.601C11.7309 15.933 12.269 15.933 12.601 15.601L18.601 9.60105C18.9329 9.2691 18.9329 8.73091 18.601 8.39897Z"
-        />
-      </svg>
     </button>
   );
 };
